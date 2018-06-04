3ca740556c88a173c82062229b9b354b75b323ca
KoelieOelie <Kevoelie@gmail.com>
2018-06-04 22:02:26 +0200

    place "RenderWebpage" function in another file

--- gitweb.php
+++ gitweb.php
@@ -1,6 +1,7 @@
 <?php 
 include"Domain.php";
 include"getwebsite.php";
+include"Render.php";
 include"$colordir/Colorize.php";
 include"$colordir/Gen.php";
 include"./Modules/Header.php";
@@ -9,8 +10,6 @@ include"./Modules/Banner.php";
 include"./Modules/Paragraph.php";
 include"./Modules/Nav.php";
 include"Modules/icon.php";
-//include"Comect.txt";
-//print($_GET["url"]);
 if(isset($_GET["url"])){
 	$url=$_GET["url"];
 }
@@ -46,7 +45,6 @@ function _LoadWebpage($string,$GitHub="https://raw.githubusercontent.com/MrCrayf
 	
 	$URL=str_replace(":extension",$extension,$URL);
 	$URL=str_replace(":Domain",$domain,$URL);
-	//echo $URL;
 
 	if($AllouwdDomain=="C"){
 		//print_r(get_headers($URL, 1));
@@ -75,87 +73,5 @@ function endsWith($haystack, $needle)
 
     return $length === 0 || 
     (substr($haystack, -$length) === $needle);
-}
-
-
-function _RenderWebpage($data){
-	$dataBuffer="";
-	//echo "<pre>";
-	//print_r($data);
-	//echo "</pre>";
-	//echo"<hr>";
-	$DataGala=sizeof($data)-1;
-	for($i=0;$i<=$DataGala;$i++){
-		if(startsWith($data[$i],"#")){
-			$CMod=str_replace("#","",$data[$i]);
-			$cleanedstr = preg_replace(
-    "/(\t|\n|\v|\f|\r| |\xC2\x85|\xc2\xa0|\xe1\xa0\x8e|\xe2\x80[\x80-\x8D]|\xe2\x80\xa8|\xe2\x80\xa9|\xe2\x80\xaF|\xe2\x81\x9f|\xe2\x81\xa0|\xe3\x80\x80|\xef\xbb\xbf)+/",
-    "",
-    $CMod
-);
-			switch ($cleanedstr) {
-				case "footer":
-						$input=array();
-						for($y=1;$y<=4;$y++){
-							if(isset($data[$i+$y])){
-								array_push($input,$data[$i+$y]);
-							}
-						}
-						$dataBuffer.=Footer($input);
-					break;
-				case "header":
-						$input=array();
-						for($y=1;$y<=4;$y++){
-							if(isset($data[$i+$y])){
-								array_push($input,$data[$i+$y]);
-							}
-						}
-						//$dataBuffer.=HeaderFun($input);
-					break;
-				case "banner":
-						$input=array();
-						for($y=1;$y<=2;$y++){
-							if(isset($data[$i+$y])){
-								array_push($input,$data[$i+$y]);
-							}
-						}
-						$dataBuffer.=banner($input);
-					break;
-				case "paragraph":
-						$input=array();
-						for($y=1;$y<=3;$y++){
-							if(isset($data[$i+$y])){
-								array_push($input,$data[$i+$y]);
-							}
-						}
-						$dataBuffer.=paragraph($input);
-					break;
-				case "divider":
-					
-						$input=array();
-						for($y=1;$y<=2;$y++){
-							if(isset($data[$i+$y])){
-								array_push($input,$data[$i+$y]);
-							}
-						}
-						//$dataBuffer.=paragraph($input);
-					break;
-				case "navigation":					
-						$input=array();
-						for($y=1;$y<=30;$y++){
-							if(isset($data[$i+$y])){
-								if(startsWith($data[$i+$y],"item-")){
-								array_push($input,$data[$i+$y]);
-								}
-							}
-						}
-						$dataBuffer.=nav($input);
-					break;
-				default:
-					$dataBuffer.="<span>".$cleanedstr.":This Module is not yet adding to this php file, Sorry. work on it ;-)</span>"."<br>";
-			}
-		}
-	}
-	return$dataBuffer;
 }
 ?>
