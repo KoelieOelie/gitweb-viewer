9786f3a2d988e79c389d56a2796d0bdb83fbb779
Unknown <Kevoelie@gmail.com>
2018-06-25 18:42:42 +0200

    Checking part 2-1 and icon update

--- TestUrl.php
+++ TestUrl.php
@@ -14,8 +14,11 @@ if (isset($_GET['Debug'])) {
         <?php if ($_POST["url"]!=""){
           $url=new URL($_POST["url"]);
           echo $url->GetGitHubUrl();
-          echo $url->theme;
-        } ?>
+          ?>
+
+          <img src="<?php echo $url->icon; ?>" alt="">
+
+        <?php } ?>
         <input type="text" name="url" value=""><button type="submit" name="Test">Test de url</button>
       </form>
     </body>
@@ -31,6 +34,7 @@ class URL
   private $directory="";
   private $github="https://raw.githubusercontent.com/MrCrayfish/GitWeb-Sites/master/:extension/:domain/:directoryindex";
   public $theme="website";
+  public $icon="";
   function __construct($url)
   {
     $s1 = explode(".", $url);
@@ -43,33 +47,55 @@ class URL
     $this->github=str_replace(":directory",$this->directory,$this->github);
     $this->CheckUrl();
   }
+
+
+  function CheckUrl(){
+
+    if($this->extension($this->extension)==true){
+      $array = get_headers($this->github);
+      Print_r($array);
+      $string = $array[0];
+      if(strpos($string,"200")){
+        $this->cicon("ok");
+      }if(strpos($string,"404")) {
+        $this->theme="notFond";
+        $this->MakeUrl("./Data/error/404");
+        $this->cicon("404");
+      }
+    }
+  	if($this->extension($this->extension)==false){
+      if($this->extension=="brouwser"){
+        $this->theme="gitwebgui";
+        $this->MakeUrl("./Data/:extension/:domain/index");
+        $this->cicon("ok");
+      }else {
+        $this->theme="notExapet";
+        $this->MakeUrl("./Data/error/404D");
+        $this->cicon("404");
+      }
+    }
+  }
+
+  function cicon($icon)
+  {
+    $Enable="https://raw.githubusercontent.com/MrCrayfish/DeviceMod-CertifiedApps/master/assets/cdm/gitweb/icon.png";
+    $Disable="./Asetes/textures/icons/disable.png";
+    if ($icon=="404") {
+      $this->icon=$Disable;
+    }
+    if ($icon=="ok") {
+      $this->icon=$Enable;
+    }
+  }
+
+
   function MakeUrl($newGitHubUrl){
     $this->github=$newGitHubUrl;
     $this->github=str_replace(":domain",$this->domain,$this->github);
     $this->github=str_replace(":extension",$this->extension,$this->github);
     $this->github=str_replace(":directory",$this->directory,$this->github);
   }
-  function CheckUrl(){
-  	if($this->extension($this->extension)!=true){
-      if($this->extension=="brouwser"){
-        $this->theme="gitwebgui";
-        $this->MakeUrl("./Data/:extension/:domain/index");
-      }else {
-        $this->theme="notExapet";
-        $this->MakeUrl("./Data/error/404D");
-      }
-    }else {
-      $array = get_headers($this->github);
-      //Print_r($array);
-      $string = $array[0];
-      if(!strpos($string,"200"))
-        //als het niet besiaat geef dan één error
-        $this->theme="notFound";
-        $this->MakeUrl("./Data/error/404");
-      }
-    }
-  }
-  function extension($extension,$extensions=array("official","app","wiki","craft","web")){
+    function extension($extension,$extensions=array("official","app","wiki","craft","web")){
   	//print_r(in_array($Domain,$Domains));
   	if(in_array($extension,$extensions)=="1"){
   		return true;
