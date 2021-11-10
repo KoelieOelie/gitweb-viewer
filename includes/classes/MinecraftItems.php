c35f06d0457788a6542a986e8483f977dc919e04
KoelieOelie <Kevoelie@gmail.com>
2021-11-10 18:50:57 +0100

    Rename "Unknow Data" to "Unknow minecraft id" in MinecraftItems.php

--- includes/classes/MinecraftItems.php
+++ includes/classes/MinecraftItems.php
@@ -15,7 +15,7 @@ class MinecraftItems
     foreach ($content["items"] as $item) {
       if (isset($item["id"])) {
         $this->content[$item["id"]]=array("src"=>$item["texture"],"label"=>$item["readable"]);
-      }      
+      }
     }
   }
   function __construct($version="1.12",$dir="static/assets/mcitems")
@@ -32,7 +32,9 @@ class MinecraftItems
   public function get_item($value)
   {
 
-    return (isset($this->content[$value])) ? $this->content[$value] : array("src"=>"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAACrSURBVFhH7ZfRCYAwDESruFX36RL97l796VxKgicSKFoVY7AP5MD+JNc70CGEMDtFxlXV2ByIMfKLt0gpsao70AfoAzS3AOmVtLboMy2YVj0EE2PTUgorkOdnseOA3Nx7zwpyzqy18xr2akib7beju6eHHGq9f8JOBiS1Vpy9e6DuwO3vgaubw0F7LQBIv2xFK3YdQO8pA8jBFew6AP6bgafoA6gP8Pe/Y+cWdMtG+zHVpVwAAAAASUVORK5CYII=","label"=>"Unknow Data");
+    return (isset($this->content[$value])) ? $this->content[$value] : array("src"=>"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QA".
+    "AAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAACrSURBVFhH7ZfRCYAwDESruFX36RL97l796VxKgicSKFoVY7AP5MD+JNc70CGEMDtFxlXV2ByIMfKLt0gpsao70AfoAzS3AOmVtLboMy2YVj0EE2P".
+    "TUgorkOdnseOA3Nx7zwpyzqy18xr2akib7beju6eHHGq9f8JOBiS1Vpy9e6DuwO3vgaubw0F7LQBIv2xFK3YdQO8pA8jBFew6AP6bgafoA6gP8Pe/Y+cWdMtG+zHVpVwAAAAASUVORK5CYII=","label"=>"Unknow minecraft id");
   }
 
 }
